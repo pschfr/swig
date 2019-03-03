a1700351dc0c8e433f65d1966f502f30e9a98ecf
Paul Schaefer <yeppaul2@gmail.com>
2019-03-02 19:01:48 -0500

    Added simple index and layout templates with the year in the footer thanks to datetime.

--- swig/__init__.py
+++ swig/__init__.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Used to pass variables to every template
+@app.context_processor
+def inject_now():
+    return { 'now': datetime.utcnow() }
+
+# On main index route, or /<name>...
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/<name>')
+def index(name=None):
+    # Render index template, pass name to page.
+    return render_template('index.html', name=name)
